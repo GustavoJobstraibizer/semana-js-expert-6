d0ddae0cb5220c19d67655802e93f7070c2b7964
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2022-03-17 17:51:33 -0300

    feat: Route layer

---
+++ server/routes.js
@@ -0,0 +1,90 @@
+import { once } from "events";
+import config from "./config.js";
+import { Controller } from "./controller.js";
+import { logger } from "./utils.js";
+const { location, pages: { homeHTML, controllerHTML }, constants: { CONTENT_TYPE } } = config
+
+const controller = new Controller()
+async function routes(request, response) {
+    const { method, url } = request;
+
+    if (method === 'GET' && url === '/') {
+        response.writeHead(302, {
+            'Location': location.home
+        })
+
+        return response.end()
+    }
+
+    if (method === 'GET' && url === '/home') {
+        const { stream } = await controller.getFileStream(homeHTML)
+
+        // padrao do response já é text/html
+        // response.writeHead(200, {
+        //     'Content-Type': 'text/html'
+        // })
+    
+        return stream.pipe(response)
+    }
+
+    if (method === 'GET' && url === '/controller') {
+        const { stream } = await controller.getFileStream(controllerHTML)
+        return stream.pipe(response)
+    }
+
+    if (method === 'GET' && url.includes('/stream')) {
+        const { stream, onClose } = controller.createClientStream()
+        request.once('close', onClose)
+        response.writeHead(200, {
+            'Content-Type': 'audio/mpeg',
+            'Accept-Ranges': 'bytes',
+        })
+
+        return stream.pipe(response)
+    }
+
+    // files (css, images, js)
+    if (method === 'GET') {
+        const {
+            stream,
+            type
+        } = await controller.getFileStream(url)
+
+        const contentType = CONTENT_TYPE[type]
+
+        if (contentType) {
+            response.writeHead(200, {
+                'Content-Type': contentType
+            })
+        }
+
+        return stream.pipe(response);
+    }
+
+    if (method === 'POST' && url === '/controller') {
+        const data = await once(request, 'data')
+        const item = JSON.parse(data)
+        const result = await controller.handleCommand(item)
+
+        return response.end(JSON.stringify(result))
+    }
+
+    response.writeHead(404)
+    return response.end()
+}
+
+function handleError(error, response) {
+    if (error.message.includes('ENOENT')) {
+        logger.warn(`asset no found ${error.stack}`)
+        response.writeHead(404)
+        return response.end()
+    }
+
+    logger.error(`caught error on API ${error.stack}`)
+    response.writeHead(500)
+    return response.end()
+}
+
+export function handler(request, response) {
+    return routes(request, response).catch(error => handleError(error, response))
+}
